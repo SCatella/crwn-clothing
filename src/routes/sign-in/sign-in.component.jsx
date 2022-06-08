cd94d3134b50b0e7c7460343e7c6e37b1f6460df
SCatella <samuel.catella@gmail.com>
2022-06-08 14:02:53 -0600

    tied in firebase auth and db to the sign in feature.

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
-import { signInWithGooglePopup } from '../../utils/firebase/firebase.utils'
+import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 
 import './sign-in.styles.scss'
 
 const SignIn = () => {
   const logGoogleUser = async () => {
-    const response = await signInWithGooglePopup();
-    console.log(response);
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user)
   }
   return (
    <div>
